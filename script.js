8b20b1d8b32b60e775f61e2310d772a5fca6f013
CaioKN7 <galdinocaiost@gmail.com>
2023-07-19 21:56:04 -0300

    Prática extra executada

--- script.js
+++ script.js
@@ -29,8 +29,23 @@
 
 //Prática 3
 
-const nome = prompt ("digite seu nome")
-   const corFavorita = prompt ("digite sua cor favorita")
-    console.log(`A cor favorita de ${nome.toUpperCase()} é ${corFavorita}.`)
-    console.log(nome.length)
-    console.log(nome.includes("a"))
+//const nome = prompt ("digite seu nome")
+//   const corFavorita = prompt ("digite sua cor favorita")
+//    console.log(`A cor favorita de ${nome.toUpperCase()} é ${corFavorita}.`)
+//    console.log(nome.length)
+//    console.log(nome.includes("a"))
+
+
+//Prática extra
+
+function verificaLogin (){
+
+  const nome = prompt ("digite seu nome")
+  const email = prompt ("digite seu melhor e-mail")
+  const frase = (`O e-mail ${email} foi cadastrado\nBoas vindas ${nome}. A quantidade de caracteres no seu nome é ${nome.length}!`)
+  console.log(frase.replaceAll("r", "x"))
+  console.log(`O email ${email} possui o caracter "@"? ${email.includes("@")}` )
+}
+
+
+verificaLogin()
