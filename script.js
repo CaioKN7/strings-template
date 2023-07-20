0e4e5df67a80d77e5d856661c86bcf169b4a18f9
CaioKN7 <galdinocaiost@gmail.com>
2023-07-19 21:37:51 -0300

    práticas 1, 2 e 3

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-function imprimirPessoa (){
-    //digite a sua solução aqui
-}
+//Prática 1
 
-imprimirPessoa()
+//function imprimirPessoa (){
+
+//    const nome = prompt ("digite seu nome")
+//    const corFavorita = prompt ("digite sua cor favorita")
+//    console.log("A cor favorita de " + nome + " é " + corFavorita + ".")
+
+
+//    console.log(`A cor favorita de ${nome} é ${corFavorita}.`)
+//}
+
+//imprimirPessoa()
+
+//Prática 2
+
+//function imprimirPessoa (){
+
+//    const nome = prompt ("digite seu nome")
+//    const corFavorita = prompt ("digite sua cor favorita")
+//    const citacao = prompt("Digite sua citação favorita")
+//    console.log(`A cor favorita de ${nome} é ${corFavorita}.`)
+//    console.log("\""+citacao+"\"")
+//    console.log(`Nome: ${nome} \nCor favorita: ${corFavorita}`)
+//    \"
+//}
+
+//imprimirPessoa()
+
+//Prática 3
+
+const nome = prompt ("digite seu nome")
+   const corFavorita = prompt ("digite sua cor favorita")
+    console.log(`A cor favorita de ${nome.toUpperCase()} é ${corFavorita}.`)
+    console.log(nome.length)
+    console.log(nome.includes("a"))
